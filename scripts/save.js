142d7e39d813fb5360039b297aa737d7531d8415
vazovsky <46650184+sadlama@users.noreply.github.com>
2020-10-07 14:05:54 +0300

    Add files via upload
    initial commit

---
+++ scripts/save.js
@@ -0,0 +1,6 @@
+if (localStorage.getItem('text_in_editor') != null) {
+    document.getElementById('editor').innerHTML = localStorage.getItem('text_in_editor')
+}
+document.addEventListener('keydown', function(e) {
+    localStorage.setItem('text_in_editor', document.getElementById('editor').innerHTML)
+})
